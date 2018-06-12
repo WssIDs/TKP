efbb0bbc60f813d3148afdb04161c60bd9dcf920
WssIDs <wssids49@gmail.com>
2018-06-12 15:48:47 +0300

    Самостоятельная работа 2

---
+++ CSharpClient_SR_02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClient_SR_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var atlPRType = Type.GetTypeFromProgID("SR_ATL2.FN01.1");
+
+            if (atlPRType != null)
+            {
+                var obj = Activator.CreateInstance(atlPRType);
+
+                if (obj != null)
+                {
+
+                    object[] methodDataFun11 = null;
+                    methodDataFun11 = new object[2];
+                    methodDataFun11[0] = 10;
+                    methodDataFun11[1] = 30;
+
+                    int resInt = (int)(atlPRType.InvokeMember("Fun11", BindingFlags.InvokeMethod, null, obj, methodDataFun11));
+
+                    object[] methodDataFun12 = null;
+                    methodDataFun12 = new object[3];
+                    methodDataFun12[0] = 20;
+                    methodDataFun12[1] = 30;
+                    methodDataFun12[2] = 50;
+
+                    float resFlt = (float)(atlPRType.InvokeMember("Fun12", BindingFlags.InvokeMethod, null, obj, methodDataFun12));
+
+                    object[] methodDataFun13 = null;
+                    methodDataFun13 = new object[1];
+                    methodDataFun13[0] = 10;
+
+                    double resDbl = (double)(atlPRType.InvokeMember("Fun13", BindingFlags.InvokeMethod, null, obj, methodDataFun13));
+
+                    Console.WriteLine("Fun11 10+30= " + resInt.ToString());
+                    Console.WriteLine("Fun12 20+30+50= " + resFlt.ToString());
+                    Console.WriteLine("Fun13 10*10= " + resDbl.ToString());
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ VBasic_SR_02/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim obj As Object
+        ' Создание объекта с указанным ProgId
+        obj = CreateObject("SR_ATL2.FN01.1")
+        Dim resInt = obj.Fun11(10, 30)
+        Console.WriteLine("Fun11 10+30= " + resInt.ToString())
+        Dim resFlt = obj.Fun12(5, 7, 12)
+        Console.WriteLine("Fun12 5+7+12= " + resFlt.ToString())
+        Dim resDbl = obj.Fun13(10)
+        Console.WriteLine("Fun13 10*10= " + resDbl.ToString())
+        Console.ReadKey()
+    End Sub
+
+End Module
